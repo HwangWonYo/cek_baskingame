62023df00420d196a8577352f07f18da79d358d4
wonyoHwang <hollal0726@gmail.com>
2018-05-17 16:46:22 +0900

    FirstCommit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_clova import Clova, question, statement
+
+app = Flask(__name__)
+clova = Clova(app, '/')
+
+@clova.launch
+def launch():
+    return question('시작했습니다')
+
+@clova.intent('HelloIntent')
+def play_game():
+    speech = "안녕하세요"
+    return statement(speech).add_speech("Hello", lang='en')
+
+
+if __name__ == "__main__":
+    app.confing['CLOVA_VERIFY_REQUESTS'] = False
+    app.run(host='0.0.0.0', port='80')
