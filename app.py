a845c888417344bb7ba06957606a1eaab4e31da8
wonyoHwang <hollal0726@gmail.com>
2018-05-18 11:26:40 +0900

    Init soragodong app

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
+"""
+    Extension server for several services
+    1. baskin robins
+    2. japan word study
+    3. sora godong nim
+
+    Mainly use Flask and flask-clova module
+    Two applications are registered by blueprint
+"""
 import logging
 from flask import Flask
 
 from baskin.main import app as baskin_app
 from jpstudy.main import app as jpstudy_app
+from soragodong.main import app as godong_app
 
 app = Flask(__name__)
 app.register_blueprint(baskin_app)
 app.register_blueprint(jpstudy_app)
+app.register_blueprint(godong_app)
 
 logging.getLogger('flask_clova').setLevel(logging.DEBUG)
 
---
+++ soragodong/main.py
@@ -0,0 +1,27 @@
+from flask import Blueprint
+from flask_clova import Clova, question, statement, session
+
+
+app = Blueprint('soragodong_api', __name__, url_prefix="/soragodong")
+clova = Clova(blueprint=app)
+
+import random
+@clova.launch
+def launch():
+    speech = random.choice(dialog_set)
+    return statement(speech)
+
+dialog_set = [
+    "하지마",
+    "가만히 있어",
+    "그것도 안돼",
+    "안돼",
+    "다시 한번 물어봐",
+    "그래",
+    "좋아",
+    "허락한다",
+    "하도록 해",
+    "그래",
+    "그래",
+    "굶어"
+]
